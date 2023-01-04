d362442cab08501161ea673a55b8f07e53964793
fast <faustogarcia1357@gmail.com>
2023-01-04 11:28:26 -0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { SeccionPh } from './components/seccion-ph/seccionPh'
+import './css/App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <SeccionPh/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/seccion-ph/SeccionPh.jsx
@@ -0,0 +1,26 @@
+import './Seccionph.css'
+
+
+export function SeccionPh (){
+    return(
+        <>
+           <section className="seccionImagen">
+                <img src={'/src/messi.png'} className="seccionImagen__img"/> 
+                <img src={'/src/messi2.png'} className="seccionImagen__img"/> 
+                <div className='seccionImagen-container-left'>
+                    <div className="seccionImagen__div">Build your metavers look</div>
+                    <div className="seccionImagen__div-2"></div>
+                </div>
+                <div className='seccionImagen-container-right'>
+                     <img src={'/src/logo.jpg'} className="seccionImagen__img"/>
+                     <div className='container-right-line'>
+                            <div className='line'></div>
+                            <h5 className='right-text'>Cooperate as designer/media</h5>
+                     </div>
+                </div>
+               
+                
+            </section>
+        </>
+    )
+} 
